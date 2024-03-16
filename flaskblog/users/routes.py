14708e065453e31c3d9dd18eadf4bab1c614bde9
MyroslavG <realmyros@gmail.com>
2024-03-15 22:20:48 -0400

    minor changes 3

--- flaskblog/users/routes.py
+++ flaskblog/users/routes.py
@@ -40,7 +40,7 @@ def login():
 
     # login_user(user, remember=True)
     access_token = create_access_token(identity=email)
-    return jsonify({'message': 'Login successful', 'access_token': access_token, 'username': user.username}), 200
+    return jsonify({'message': 'Login successful'}), 200 
 
     # user = User.query.filter_by(email=email).first()
     # if user and bcrypt.check_password_hash(user.password, password):
