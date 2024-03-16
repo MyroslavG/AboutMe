a23b68998710eab2ab700e26cc72d73322274ab3
MyroslavG <realmyros@gmail.com>
2024-03-15 20:47:51 -0400

    minor changes 2

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -6,6 +6,7 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from flask_migrate import Migrate
 from dotenv import load_dotenv
+from flask_jwt_extended import JWTManager, create_access_token
 # from flask_cors import CORS
 #from flaskblog.posts.s3_utils import upload_to_s3
 #from flask_socketio import SocketIO
@@ -15,6 +16,7 @@ app = Flask(__name__)
 load_dotenv()
 
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
 
 S3_BUCKET_NAME = os.getenv('S3_BUCKET_NAME')
 AWS_ACCESS_KEY = os.getenv('AWS_ACCESS_KEY')
@@ -30,6 +32,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql://{DB_USERNAME}:{DB_PASSWORD}@{D
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
@@ -39,7 +42,6 @@ app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS')
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
 mail = Mail(app)
-#socketio = SocketIO(app)
 
 from flaskblog.users.routes import users
 from flaskblog.posts.routes import posts
@@ -47,8 +49,15 @@ from flaskblog.posts.routes import posts
 app.register_blueprint(users)
 app.register_blueprint(posts)
 
+app.app_context().push() # when initializing the datatabase
 
-#from flaskblog.messages.routes import socketio_bp
+# from flaskblog import db
+# db.create_all()
+# from flaskblog.models import User
+# user = User(username='XXXXX', email='XXXXXXXXXXXXXXX', password='XXXXX')
+# db.session.add(user)
+# db.session.commit()
 
-# Register the socketio_bp blueprint with the SocketIO instance
-#socketio.register_blueprint(socketio_bp)
+# flask db init
+# flask db migrate -m "Initial commit"
+# flask db upgrade
--- flaskblog/users/routes.py
+++ flaskblog/users/routes.py
@@ -7,6 +7,7 @@ from flaskblog.users.forms import (RegistrationForm, LoginForm, UpdateAccountFor
 from flaskblog.users.utils import save_picture, send_reset_email
 from flask_wtf.csrf import generate_csrf
 from sqlalchemy import and_
+from flask_jwt_extended import create_access_token
 
 users = Blueprint('users', __name__)
 
@@ -40,7 +41,8 @@ def login():
     user = User.query.filter_by(email=email).first()
     if user and bcrypt.check_password_hash(user.password, password):
         login_user(user, remember=True)
-        return jsonify({'message': 'Login successful', 'username': user.username}), 200
+        access_token = create_access_token(identity=email)
+        return jsonify({'message': 'Login successful', 'access_token': access_token, 'username': user.username}), 200
     else:    
         return jsonify({'message': 'Login unsuccessful. Please check email and password'}), 401
     
